b906a5744cf9ddc8ad82b431fb080e724afa045c
suelenfeijo <suelenfeijobr@gmail.com>
2024-01-16 13:34:44 -0300

    add rota for tecnicos list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,17 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
+import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
 const routes: Routes = [{
 
   //quando o path for vazio oque renderiza é NavComponent
   path:'',component: NavComponent, 
   //a home é filha do path vazio , ela herda oque é renderizado no path '' , e é renderizado dentro dele
-  children: [{path: 'home', component: HomeComponent}
+  children: 
+  [{path: 'home', component: HomeComponent},
+  //tecnicos -> filho de NavComponent, funciona igual home
+  {path: 'tecnicos', component: TecnicoListComponent}
   ]},
 ];
 
