3614c535887c085a761eec748f91d3475a237075
suelenfeijo <suelenfeijobr@gmail.com>
2024-01-17 19:34:20 -0300

    rota para tecnico-create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './auth/auth.guard';
 import { DecorationComponent } from './components/decoration/decoration.component';
+import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 
 const routes: Routes = [
   //quando o path for login oque renderiza é LoginComponent
@@ -13,11 +14,15 @@ const routes: Routes = [
   {path:'',component: DecorationComponent },
   //quando o path for vazio oque renderiza é NavComponent
   {path:'inicio',component: NavComponent, canActivate: [AuthGuard] ,   
-
   //a home é filha do path vazio , ela herda oque é renderizado no path '' , e é renderizado dentro dele
-children:   [{path: 'home', component: HomeComponent},
+  children:   [{path: 'home', component: HomeComponent},
+
+
+
   //tecnicos -> filho de NavComponent, funciona igual home
-  {path: 'tecnicos', component: TecnicoListComponent}
+  {path: 'tecnicos', component: TecnicoListComponent},
+  {path: 'tecnicos/create', component: TecnicoCreateComponent}
+
   ]},
 ];
 
