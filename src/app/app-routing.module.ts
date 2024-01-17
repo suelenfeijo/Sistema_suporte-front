fc12b46f606fef3b8cda6b80122a999cf7b58562
suelenfeijo <suelenfeijobr@gmail.com>
2024-01-17 05:31:35 -0300

    rota para login add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [{
-
+const routes: Routes = [
+  //quando o path for login oque renderiza é LoginComponent
+  {path:'login', component:LoginComponent},
   //quando o path for vazio oque renderiza é NavComponent
-  path:'',component: NavComponent, 
+  {path:'',component: NavComponent, 
   //a home é filha do path vazio , ela herda oque é renderizado no path '' , e é renderizado dentro dele
   children: 
   [{path: 'home', component: HomeComponent},
