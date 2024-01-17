c0aad64971cbd7cf850b8bdd590b1e626188ae5c
suelenfeijo <suelenfeijobr@gmail.com>
2024-01-17 06:34:28 -0300

    validação de email e senha com angular - parte 1

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { Credenciais } from 'src/app/models/credenciais';
 
 @Component({
@@ -12,6 +13,12 @@ export class LoginComponent implements OnInit {
     email: '',
     senha: ''
   }
+
+  //primeiro pparametro é any , e o segundo nesse caso, é usado o validators para validar email
+  email = new FormControl(null, Validators.email);
+  //valida se o campo senha tem o min de 3 caracters
+  senha = new FormControl(null, Validators.minLength(3));
+
   constructor() { }
 
   ngOnInit(): void {
