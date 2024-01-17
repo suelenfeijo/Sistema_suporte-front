ec4fc3621e97a566eff910464ca466c161b854cf
suelenfeijo <suelenfeijobr@gmail.com>
2024-01-17 04:56:04 -0300

    interface em model técnico add

---
+++ src/app/models/tecnicos.ts
@@ -0,0 +1,11 @@
+export interface Tecnico{
+    //a ? define que pode ser um dado que possa ou não estar presente, e o any, qualquer (string ou número)
+    id?: any;
+    nome:string;
+    cpf: string;
+    email:string;
+    senha:string;
+    perfis:string[];
+    dataCriacao: any;
+
+}
