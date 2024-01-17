06228fcd2e50b6ebc05f4ad6989eae6ba8236a9d
suelenfeijo <suelenfeijobr@gmail.com>
2024-01-17 07:38:48 -0300

    auth service implementado

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Credenciais } from '../models/credenciais';
+import { HttpClient } from '@angular/common/http';
+import { API_CONFIG } from '../config/Api.config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http:  HttpClient) { }
+
+  authenticate(creds: Credenciais){
+    return this.http.post(`${API_CONFIG.baseUrl}/login`, creds, {
+      observe: 'response',
+      responseType: 'text'
+    });
+  }
 }
