621e815f301299585b3c7d39ce33624e33603e2f
suelenfeijo <suelenfeijobr@gmail.com>
2024-01-18 21:36:39 -0300

    bloqueio de rota e retorno ao login caso não autenticado

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRoute, ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
+import { ActiveDescendantKeyManager } from '@angular/cdk/a11y';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private route: ActivatedRoute,private router: Router ) {}
 
   canActivate( route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     let authenticated = this.authService.isAuthenticated();
@@ -16,7 +17,8 @@ export class AuthGuard implements CanActivate {
     if(authenticated) {
       return true;
     } else {
-      this.router.navigate(['decoration']);
+      this.router.navigate(['/login'], {relativeTo: this.route});
+
       return false
     }
   }
