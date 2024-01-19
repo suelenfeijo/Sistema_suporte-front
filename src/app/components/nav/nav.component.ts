b45932bb2311283622838c3420c6241c77256353
suelenfeijo <suelenfeijobr@gmail.com>
2024-01-18 21:38:46 -0300

    navegação para chamados/read/1

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -12,12 +12,13 @@ export class NavComponent implements OnInit {
 
   constructor(private router: Router,
     private authService: AuthService,
-    private toast: ToastrService) { }
+    private toast: ToastrService,
+    private route: ActivatedRoute) { }
 
   //quando esse componente é iniciado , tudo dentro desse método inicia junto
   ngOnInit(): void {
     //quando esse componente for iniciado, navegue para o path home
-    this.router.navigate(['chamados/create'])
+    this.router.navigate(['chamados/read/1'], {relativeTo: this.route});
   }
 
   logout(){
