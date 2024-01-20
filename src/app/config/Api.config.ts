0fc978bcbb903be826cc24c56c208551482460bb
suelenfeijo <suelenfeijobr@gmail.com>
2024-01-20 11:59:36 -0300

    add conexão api no deploy ec2 amazon via ipv4

--- src/app/config/Api.config.ts
+++ src/app/config/Api.config.ts
@@ -1,5 +1,6 @@
 export const API_CONFIG ={
 // baseUrl :'https://helpdesk-back.onrender.com'
-     baseUrl :'http://localhost:8090'
+// baseUrl :'http://localhost:8090'
+baseUrl :'http://54.226.239.246:8090'
 
 }
